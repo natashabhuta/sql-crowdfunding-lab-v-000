c6b385e8a4a495ff092b20945f6ac1206bf5c988
natashabhuta <natasha.bhuta@gmail.com>
2018-08-07 22:34:05 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,7 +16,7 @@ end
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "SELECT users.name, users.age, amount.pledges FROM users 
   INNER JOIN pledges
-  ON users.id = pledges
+  ON users.id = pledges.user_id 
   GROUP BY users.name"
 end
 
